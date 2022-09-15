68907a0f0ca67bec5c395ed7aa2347b0cb6893f8
MaianeSamara <maianesamara@gmail.com>
2022-09-15 18:19:33 -0300

    Correção Exercício de Fixaçao Saída de dados em Java

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,14 @@ public class Main {
 		double price2 = 650.0;
 		double masure = 53.234567;
 
+		System.out.println("Products");
 		System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
 		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
-		
-		System.out.printf("Record: %s old, code %d and gender: %s", age, code, gender);
-		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
-		
+		System.out.println();
+		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+		System.out.println();
 		System.out.printf("Measue with eight decimal places: %.8f%n", masure);
 		System.out.printf("Rouded (three decimal places): %.3f%n", masure);
-		
 		Locale.setDefault(Locale.US);
 		System.out.printf("US decimal point: %.3f%n", masure);
 	}
