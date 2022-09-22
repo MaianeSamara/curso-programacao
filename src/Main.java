04099f282c777ae1f50fd602080b9f929aaf58b1
MaianeSamara <maianesamara@gmail.com>
2022-09-22 15:01:33 -0300

    Estrutura Condicional if-else - Exercicio 04

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,21 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
 
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
 
-		if (A % B == 0 || B % A == 0) {
-			System.out.println("SÃO MULTIPLOS");
+		int duracao;
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
 		}
 
 		else {
-			System.out.println("NÃO SÃO MULTIPLOS");
-		} 
-	
+			duracao = 24 - horaInicial + horaFinal;
+		}
+
+		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
 		sc.close();
 	}
 }
