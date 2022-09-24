6cd94afe642d7e15107d858097dfb1e0ac290baa
MaianeSamara <maianesamara@gmail.com>
2022-09-24 15:24:39 -0300

    Sintaxe opcional - Switch-case

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,45 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int minutos = sc.nextInt();
+		int x = sc.nextInt();
+		String dia;
+
+		switch (x) {
+		case 1:
+			dia = "domingo";
+			break;
+
+		case 2:
+			dia = "segunda";
+			break;
+
+		case 3:
+			dia = "terca";
+			break;
+
+		case 4:
+			dia = "quarta";
+			break;
+
+		case 5:
+			dia = "quinta";
+			break;
+
+		case 6:
+			dia = "sexta";
+			break;
+
+		case 7:
+			dia = "sabado";
+			break;
 		
-		double conta = 50.0;
-		if (minutos > 100) {
-			conta += (minutos - 100) *2.0; 
+		default:
+			dia = "valor invalido";
+			break;
 		}
-		
-		System.out.printf("Valor da conta = R$ %.2f%n", conta);
-		
-		
+
+		System.out.println("Dia da semana: " + dia);
+
 		sc.close();
-		}
+	}
 }
