3f283a6f5c7df4acf94a0895bcc1d4ff8c1d8fcb
MaianeSamara <maianesamara@gmail.com>
2022-09-26 15:20:52 -0300

    Estrutura repetitiva Enquanto (While)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		double largura = sc.nextDouble();
-		double comprimento = sc.nextDouble();
-		double metroQuadrado = sc.nextDouble();
+		int x = sc.nextInt();
+		
+		int soma = 0;
+		while (x !=0) {
+			soma = soma + x;
+			x = sc.nextInt();
+		}
 
-		double area = largura * comprimento;
-		double preco = area * metroQuadrado;
-
-		System.out.printf("AREA = %.2f%n", area);
-		System.out.printf("PRECO = %.2f%n", preco);
+		System.out.println(soma);
 		
 		sc.close();
 	}
