a4fa0098a6435e78179a19f9abda7dbfcaa30df6
MaianeSamara <maianesamara@gmail.com>
2022-09-28 17:08:56 -0300

    Estrutura Repetitiva for - Exercicio 01

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
 		
-		int soma = 0;
-		for (int i=0; i<N; i++) {
-			int x = sc.nextInt();
-			soma = soma + x;
+		int x = sc.nextInt();
+		
+		for (int i=1; i<=x; i++) {
+			if(i % 2 != 0) {
+			
+			System.out.println(i);
 		}
-		
-		System.out.println(soma);
-
-		sc.close();
+		}
+			sc.close();
 	}
 }
