6bacead1b21a6f593cf9e796461f6f13fa552589
MaianeSamara <maianesamara@gmail.com>
2022-09-29 16:33:48 -0300

    Estrutura Repetitiva for - Exercicio 06

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,15 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
-		
-		int fator = 1;
-		for (int i=1; i<=n; i++) {
-			fator = fator * i;
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+
+				System.out.println(i);
+			}
+
 		}
-				
-			System.out.println(fator);
-		
+
 		sc.close();
 	}
 }
