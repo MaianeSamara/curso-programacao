2cf554dcf74607b4538dfaa9de818d1b95acdcb8
MaianeSamara <maianesamara@gmail.com>
2022-09-30 17:48:17 -0300

    Estrutura Repetitiva faça-enquanto (do-while)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
+		char resp;
 
-		for (int i=1; i<=n; i++) {
-			
-			int linha1 = i;
-			int linha2 = i * i;
-			int linha3 = i * i * i;
+		do {
+			System.out.print("Digite a temperatura em Celsius: ");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
 
-				System.out.printf("%d %d %d%n", linha1, linha2, linha3);
-			}
+			System.out.println("Deseja repetir (s/n)");
+			resp = sc.next().charAt(0);
+		} while (resp != 'n');
 
 		sc.close();
 	}
